cfea040e68bcd36cb133c6e0103360eed9507de9
魏宗琪 <weizongqi.wzq@bytedance.com>
2021-07-02 16:12:50 +0800

    debug

--- train_fine/dataset.py
+++ train_fine/dataset.py
@@ -54,8 +54,8 @@ class AVA(torch.utils.data.Dataset):
 
     def _actualgetitem(self, idx: int):
         path = str(int(self.files.iloc[idx][0])) + ".jpg"
-        print(path,'++')
         img = Image.open(Path(self.image_dir) / path).convert("RGB")
+        print(len(img),'++')
         img = self.pad_square(img)
         if self.horizontal_flip:
             img = transforms.RandomHorizontalFlip()(img)
