2d8e97cdf7c1b7314e96834780f06c5da9344812
魏宗琪 <weizongqi.wzq@bytedance.com>
2021-07-04 15:02:25 +0800

    debug

---
+++ train_fine/getResults.py
@@ -0,0 +1,17 @@
+import os
+
+
+def getMAE():
+    f_gt = open('test_labels.csv', 'r')
+    gt_lines = f_gt.readlines()
+    f_gt.close()
+
+    f_pre = open('ckpts/IA2NIMA/AVA/output.txt','r')
+    pre_lines = f_pre.readlines()
+    f_pre.close()
+
+    print(gt_lines[0],'++')
+    print(pre_lines[0],'--')
+
+
+getMAE()
